2e77bed2f2104a45a97ac18f2ba4b0cbe6192987
Administrator <314833234@qq.com>
2019-04-26 21:05:38 +0800

    添加vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
+import Vuex from 'vuex'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
 Vue.use(VueRouter)
 Vue.use(ElementUI)
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
